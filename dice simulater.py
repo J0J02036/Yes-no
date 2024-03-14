10c77059c0463ec75d7a693b43cd036be67d0b2c
J0J02036 <159393151+J0J02036@users.noreply.github.com>
2024-03-14 14:33:27 +1300

    updated dices sim 2 die
    rolls 2 die and checks for double roll
    doubles if doubles score is yes
    on loop for testing purposes'

--- dice simulater.py
+++ dice simulater.py
@@ -1,6 +1,35 @@
 # import random
 import random
 
+
+# generates a random number between
+# to simulate a roll of a die
+
+def roll_die():
+    result = random.randint(a=1, b=6)
+    return result
+
+
+
+# main routine starts here
+
 for item in range(0, 10):
-    dice_roll = random.randint(a=1, b=3)
-    print(dice_roll, end="  ")
+        user_score = 0
+        double_score = "no"
+
+        # roll two dice
+        roll_1 = roll_die()
+        roll_2 = roll_die()
+
+        # checks for double score opportunity
+        if roll_1 == roll_2:
+            double_score = "yes"
+
+        # find total points (so far)
+        user_points = roll_1 + roll_2
+
+        # show the results
+        print()
+        print()
+        print(f"die 1: {roll_1}      die 2: {roll_2}     points: {user_points}")
+        print(f"double score opportunity: {double_score}")
